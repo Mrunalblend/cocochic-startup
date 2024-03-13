24ee37ec6122aadfd5bc5f071e7411b230bbe11d
Mrunalblend <136045721+Mrunalblend@users.noreply.github.com>
2024-03-13 15:46:54 +0530

    added files

---
+++ TinDog-Start-master/index.js
@@ -0,0 +1,16 @@
+// CSS won't work so I did this
+document.querySelector("h1").style.fontWeight = "800";
+
+document.querySelector(".navbar-brand").style.fontSize = "2.5rem";
+
+document.querySelector(".navbar-brand").style.fontWeight = "1000";  
+
+document.querySelector(".navbar").style.paddingTop = "0";
+
+document.querySelector(".navbar").style.paddingBottom = "4.5rem";
+
+document.querySelector(".container-fluid").style.padding = "2% 15% 3% 15%";
+
+document.querySelector(".feature-row").style.padding = "2% 15% 2% 15%";
+
+// CSS won't work segment end
